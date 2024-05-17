d89c26cd48d884c2a899e14a87e4fb806c058e17
jaybee-heon <changheon.whale@gmail.com>
2024-05-18 02:15:53 +0900

    fix

--- get_relevant_classes.py
+++ get_relevant_classes.py
@@ -2,17 +2,17 @@ import os
 import pickle
 
 if __name__ == "__main__":
-    with open("newest.pkl", "rb") as f:
-        data = pickle.load(f)
-        print(data)
+    projects = "./projects.txt"
 
-    for pid, vid in data.items():
-        if not os.path.exists(f"./relevant_classes/{pid}-{vid}"):
-            os.system(f"mkdir ./relevant_classes/{pid}-{vid}")
-        tmp_dir = f"/tmp/{pid}-{vid}f"
+    with open(projects) as pf:
+        for line in pf:
+            pid, vid = line.split()
+            if not os.path.exists(f"./relevant_classes/{pid}-{vid}"):
+                os.system(f"mkdir ./relevant_classes/{pid}-{vid}")
+            tmp_dir = f"/tmp/{pid}-{vid}f"
 
-        if not os.path.exists(tmp_dir):
-            os.system(f"defects4j checkout -p {pid} -v {vid}f -w {tmp_dir}")
-        os.system(f"cd {tmp_dir} && defects4j export -p classes.relevant -o relevant_classes")
-        os.system(f"mv {tmp_dir}/relevant_classes ./relevant_classes/{pid}-{vid}/relevant_classes")
-        # break
+            if not os.path.exists(tmp_dir):
+                os.system(f"defects4j checkout -p {pid} -v {vid}f -w {tmp_dir}")
+            os.system(f"cd {tmp_dir} && defects4j export -p classes.relevant -o relevant_classes")
+            os.system(f"mv {tmp_dir}/relevant_classes ./relevant_classes/{pid}-{vid}/relevant_classes")
+            # break

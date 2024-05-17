01f7b74001212fc6ddd4f1d5a3b30254772f1ddf
jaybee-heon <changheon.whale@gmail.com>
2024-05-18 01:43:46 +0900

    relevant class

---
+++ get_relevant_classes.py
@@ -0,0 +1,16 @@
+from get_coverage import *
+
+def make_relevant_classes_command(pid, vid):
+    command = f"defects4j export -p classes.relevant -w checkout/{pid}_{vid}"
+    return command
+
+if __name__ == "__main__":
+    projects = "./projects.txt"
+    with open(projects) as pf:
+        for line in pf:
+            print(f"Doing checkout...")
+            print(f"{pid}-{vid}")
+            run_command(make_checkout_command(pid, vid))
+
+            print("Export...")
+            run_command(make_relevant_classes_command(pid, vid))

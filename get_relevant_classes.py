706be0f4982f5aadd1c89856a763daf87ba2eb4f
jaybee-heon <changheon.whale@gmail.com>
2024-05-18 02:30:21 +0900

    f

--- get_relevant_classes.py
+++ get_relevant_classes.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
             if not os.path.exists(tmp_dir):
                 os.system(f"defects4j checkout -p {pid} -v {vid}f -w {tmp_dir}")
-            os.system(f"cd {tmp_dir} && defects4j export -p classes.relevant -o relevant_classes")
+            os.system(f"cd {tmp_dir} && defects4j export -p classes.relevant -o relevant_classes.txt")
             target_dir = f"/app/defects4j/relevant_classes/{pid}-{vid}"
-            os.system(f"mv {tmp_dir}/relevant_classes {target_dir}")
+            os.system(f"mv {tmp_dir}/relevant_classes.txt {target_dir}/relevant_classes.txt")
             # break

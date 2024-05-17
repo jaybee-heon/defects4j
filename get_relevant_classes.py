3eaa1c7bcb6584997ac373ef74400959a8a50f1f
jaybee-heon <changheon.whale@gmail.com>
2024-05-18 02:19:18 +0900

    fix

--- get_relevant_classes.py
+++ get_relevant_classes.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
             pid, vid = line.split()
             if not os.path.exists(f"./relevant_classes/{pid}-{vid}"):
                 os.system(f"mkdir ./relevant_classes/{pid}-{vid}")
-            tmp_dir = f"/tmp/{pid}-{vid}f"
+            tmp_dir = f"/tmp/{pid}-{vid}"
 
             if not os.path.exists(tmp_dir):
-                os.system(f"defects4j checkout -p {pid} -v {vid}f -w {tmp_dir}")
+                os.system(f"defects4j checkout -p {pid} -v {vid} -w {tmp_dir}")
             os.system(f"cd {tmp_dir} && defects4j export -p classes.relevant -o relevant_classes")
-            os.system(f"mv {tmp_dir}/relevant_classes ./relevant_classes/{pid}-{vid}/relevant_classes")
+            target_dir = f"/app/defects4j/relevant-classes/{pid}-{vid}"
+            if not os.path.exists(target_dir):
+                os.system(f"mkdir {target_dir}")
+            os.system(f"mv {tmp_dir}/relevant_classes {target_dir}/relevant_classes")
             # break
